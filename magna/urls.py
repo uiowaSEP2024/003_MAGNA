96dc7787e782fdda4adcc92bbee9462c19b78e56
Chase Dittmer <chassedittmer@gmail.com>
2024-02-04 13:56:18 -0600

    working changes on the html

--- magna/urls.py
+++ magna/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .templates import home_page
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include("home.html")),
 ]
