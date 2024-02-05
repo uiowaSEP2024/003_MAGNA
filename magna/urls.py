ce1bc91e9e6d23cab566413336b56e2dec273834
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-02-04 18:50:08 -0600

    Got the basic login form, no CSS though, it's just two html boxes and a normal page. Right now, the app loads into this.

--- magna/urls.py
+++ magna/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import login.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # root view
+    path("", login.views.login_view, name="login"),
 ]
