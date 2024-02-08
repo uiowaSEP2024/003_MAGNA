527644280c887d79bf661cbd09585d32b5c9ecb5
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-02-07 18:44:39 -0600

    added some tests for the pto form and the home page - super basic tests that will need to be expanded upon

--- forms/tests.py
+++ forms/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+class FormsTestCase(TestCase):
+
+    def test_pto_page(self):
+        response = self.client.get(reverse('pto'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pto.html')
+
+    #still needs a test for the pto form submission here
+    def test_pto_submit(self):
+        pass
--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+class HomeTestCase(TestCase):
+
+    def test_home_page(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
