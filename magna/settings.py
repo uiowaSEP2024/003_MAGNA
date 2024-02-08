b7465a38956b50584480647765c4b40e395100ba
Charles Wilmot II <chazwilmot@gmail.com>
2024-02-08 12:57:25 -0600

    static css url update

--- magna/settings.py
+++ magna/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "static/css"
 
 STATICFILES_DIRS = [
 ]
