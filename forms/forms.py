6e9865410dce5fbe05694998a4183c8464e0fde1
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-02-07 18:34:10 -0600

    PTO form data is now in, can be filled out and is properly parsed - still not passed to database though

--- forms/forms.py
+++ forms/forms.py
@@ -1,1 +1,24 @@
-from django import f
+from django import forms
+
+SHIFT_CHOICES = (
+    ("1", "morning"),
+    ("2", "evening"),
+    ("3", "night"),
+)
+
+ABSENCES_CHOICES = (
+    ("1", "PTO")
+)
+
+class PTOForm(forms.Form):
+    clock = forms.CharField()
+    shift = forms.ChoiceField(choices=SHIFT_CHOICES)
+    name = forms.CharField()
+    first_day_absent = forms.DateField()
+    last_day_absent = forms.DateField()
+    hours = forms.IntegerField()
+    absent_type = forms.ChoiceField(choices=ABSENCES_CHOICES)
+    approving_supervisor = forms.CharField()
+    email = forms.EmailField()
+
+
--- forms/views.py
+++ forms/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+
+from forms.forms import PTOForm
+
 
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html')
 
+
 def pto(request):
-    return render(request, 'pto.html')
+    if request.method == 'POST':
+        form = PTOForm(request.POST)
+        if form.is_valid():
+            request_form = authenticate(clock=form.cleaned_data['clock'],
+                                        shift=form.cleaned_data['shift'],
+                                        name=form.cleaned_data['full-name'],
+                                        first_day_absent=form.cleaned_data['first-day-absent'],
+                                        last_day_absent=form.cleaned_data['last-day-absent'],
+                                        hours=form.cleaned_data['hours'],
+                                        absent_type=form.cleaned_data['absence-type'],
+                                        approving_supervisor=form.cleaned_data['approving-supervisor'],
+                                        email=form.cleaned_data['email'])
+            if request_form is not None:
+                # test print statement please remove before deployment
+                print("Form valid")
+                return redirect('home')
+    else:
+        form = PTOForm()
+    return render(request, 'pto.html', {'form': form})
