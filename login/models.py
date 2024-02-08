5e036873824d0e8b0acd6d3133133be7d72c853a
teckenrod <treyeck2@gmail.com>
2024-02-08 16:42:27 -0600

    added initial employee model and test

--- login/models.py
+++ login/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    """Model for employees"""
+
+    pass
--- login/tests.py
+++ login/tests.py
@@ -1,8 +1,13 @@
+import pytest
 from django.test import TestCase
 from django.urls import reverse
 
+from login.models import Employee
+
 
 class LoginTestCase(TestCase):
+    """FILL IN"""
+
     def test_login_page(self):
         """FILL IN"""
         response = self.client.get(reverse("login"))
@@ -13,3 +18,21 @@ class LoginTestCase(TestCase):
         """FILL IN"""
         # Add a test for form submission here
         pass
+
+
+class EmployeeModelTestCase(TestCase):
+    """Testing class for the employee model"""
+
+    @pytest.mark.django_db
+    def test_employee_creation(self):
+        """Test employee creation model"""
+        employee = Employee.objects.create(
+            name="Test User",
+            role="employee",
+            clock_number="021456",
+            email="test@example.com",
+        )
+        assert employee.name == "Test User"
+        assert employee.role == "Developer"
+        assert employee.clock_number == "12345"
+        assert employee.email == "test@example.com"
