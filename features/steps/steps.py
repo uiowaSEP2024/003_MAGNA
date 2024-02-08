d2c2b82551830e535414ec0538c0d5396dfb33f2
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-02-08 16:55:24 -0600

    Changes to meet precommit criteria

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,6 +1,4 @@
 # steps.py
-import webbrowser
-
 from behave import given, then, when
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,50 +9,64 @@ def step_impl(context):
     """FILL IN"""
     # context.browser.visit(context.get_url("login"))
     context.browser = webdriver.Chrome()
-    context.browser.get('http://localhost:8000/')
+    context.browser.get("http://localhost:8000/")
+
 
 @given("the user has navigated to the home page")
 def step_impl(context):
     context.browser = webdriver.Chrome()
-    context.browser.get('http://localhost:8000/home')
+    context.browser.get("http://localhost:8000/home")
+
 
 @when("the user submits the login form with valid credentials")
 def step_impl(context):
     """FILL IN"""
     # context.browser.fill("username", "user")
-    username_input = context.browser.find_element(By.ID, 'username')
-    username_input.send_keys('user')
+    username_input = context.browser.find_element(By.ID, "username")
+    username_input.send_keys("user")
     # context.browser.fill("password", "pass")
-    password_input = context.browser.find_element(By.ID, 'password')
-    password_input.send_keys('pass')
-    login_button = context.browser.find_element(By.ID, 'log-in')
+    password_input = context.browser.find_element(By.ID, "password")
+    password_input.send_keys("pass")
+    login_button = context.browser.find_element(By.ID, "log-in")
     login_button.click()
+
 
 @when("the user submits the login form with incorrect credentials")
 def step_impl(context):
-    username_input = context.browser.find_element(By.ID, 'username')
-    username_input.send_keys('user_wrong')
-    password_input = context.browser.find_element(By.ID, 'password')
-    password_input.send_keys('pass_wrong')
-    login_button = context.browser.find_element(By.ID, 'log-in')
+    username_input = context.browser.find_element(By.ID, "username")
+    username_input.send_keys("user_wrong")
+    password_input = context.browser.find_element(By.ID, "password")
+    password_input.send_keys("pass_wrong")
+    login_button = context.browser.find_element(By.ID, "log-in")
     login_button.click()
+
 
 @when("the user clicks on the PTO button")
 def step_impl(context):
-    pto_button = context.browser.find_element(By.ID, 'pto-link')
+    pto_button = context.browser.find_element(By.ID, "pto-link")
     pto_button.click()
+
 
 @then("the user should not be redirected to the home page")
 def step_impl(context):
-    assert context.browser.current_url != "http://localhost:8000/home", f"Expected url to be on login page, instead is on {context.browser.current_url}"
+    assert context.browser.current_url != "http://localhost:8000/home", (
+        f"Expected url to be on login page, "
+        f"instead is on {context.browser.current_url}"
+    )
 
 
 @then("the user should be redirected to the home page")
 def step_impl(context):
     """FILL IN"""
-    assert context.browser.current_url == "http://localhost:8000/home", f"Expected url to be on home page, instead is on {context.browser.current_url}"
+    assert context.browser.current_url == "http://localhost:8000/home", (
+        f"Expected url to be on home page, "
+        f"instead is on {context.browser.current_url}"
+    )
+
 
 @then("the user is redirected to the PTO form")
 def step_impl(context):
-    assert context.browser.current_url == "http://localhost:8000/pto", f"Expected url to be on pto form page, instead is on {context.browser.current_url}"
-
+    assert context.browser.current_url == "http://localhost:8000/pto", (
+        f"Expected url to be on pto form page, "
+        f"instead is on {context.browser.current_url}"
+    )
