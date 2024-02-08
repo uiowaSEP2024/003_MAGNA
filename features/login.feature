eed3d7a07b8588c60d9259b79a280f83ab4bbfb8
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-02-08 16:34:28 -0600

    Login feature is done- but needs database link still

--- features/login.feature
+++ features/login.feature
@@ -3,3 +3,8 @@ Feature: Login functionality
     Given the user has navigated to the login page
     When the user submits the login form with valid credentials
     Then the user should be redirected to the home page
+
+  Scenario: User tries to login with incorrect credentials
+    Given the user has navigated to the login page
+    When the user submits the login form with incorrect credentials
+    Then the user should not be redirected to the home page
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,22 +1,46 @@
 # steps.py
+import webbrowser
+
 from behave import given, then, when
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 @given("the user has navigated to the login page")
 def step_impl(context):
     """FILL IN"""
-    context.browser.visit(context.get_url("login"))
-
+    # context.browser.visit(context.get_url("login"))
+    context.browser = webdriver.Chrome()
+    context.browser.get('http://localhost:8000/')
 
 @when("the user submits the login form with valid credentials")
 def step_impl(context):
     """FILL IN"""
-    context.browser.fill("username", "user")
-    context.browser.fill("password", "pass")
-    context.browser.find_by_value("Log In").first.click()
+    # context.browser.fill("username", "user")
+    username_input = context.browser.find_element(By.ID, 'username')
+    username_input.send_keys('user')
+    # context.browser.fill("password", "pass")
+    password_input = context.browser.find_element(By.ID, 'password')
+    password_input.send_keys('pass')
+    login_button = context.browser.find_element(By.ID, 'log-in')
+    login_button.click()
+
+@when("the user submits the login form with incorrect credentials")
+def step_impl(context):
+    username_input = context.browser.find_element(By.ID, 'username')
+    username_input.send_keys('user_wrong')
+    password_input = context.browser.find_element(By.ID, 'password')
+    password_input.send_keys('pass_wrong')
+    login_button = context.browser.find_element(By.ID, 'log-in')
+    login_button.click()
+
+@then("the user should not be redirected to the home page")
+def step_impl(context):
+    assert context.browser.current_url == "http://localhost:8000", f"Expected url to be on login page, instead is on {context.browser.current_url}"
 
 
 @then("the user should be redirected to the home page")
 def step_impl(context):
     """FILL IN"""
-    assert context.browser.url == context.get_url("home")
+    assert context.browser.current_url != "http://localhost:8000/home", f"Expected url to be on login page, instead is on {context.browser.current_url}"
+
