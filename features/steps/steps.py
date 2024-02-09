d62151d804ff86c3767cc3e9f6b63e2a9cf0dd73
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-02-08 18:01:12 -0600

    Added a ton of tests for the PTO feature. Be careful running these before implementation. The test currently times out since it can't get from the login to home page

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 
 @given("the user has navigated to the login page")
 def step_impl(context):
-    """FILL IN"""
+    """The step for putting the test env on the login page"""
     # context.browser.visit(context.get_url("login"))
     context.browser = webdriver.Chrome()
     context.browser.get("http://localhost:8000/")
@@ -14,13 +14,149 @@ def step_impl(context):
 
 @given("the user has navigated to the home page")
 def step_impl(context):
+    """Putting the test enviroment on the home page"""
     context.browser = webdriver.Chrome()
     context.browser.get("http://localhost:8000/home")
 
 
+@given("a user is logged in and on the PTO form")
+def step_impl(context):
+    """Logs the user in, then navigates to the PTO Form
+    This is a temporary fix until I can get a before run function set up.
+    It didn't want to work earlier
+    """
+    context.browser = webdriver.Chrome()
+    context.browser.get("http://localhost:8000/")
+    username_input = context.browser.find_element(By.ID, "username")
+    username_input.send_keys("user")
+    password_input = context.browser.find_element(By.ID, "password")
+    password_input.send_keys("pass")
+    login_button = context.browser.find_element(By.ID, "log-in")
+    login_button.click()
+    # then once we're on the home page
+    pto_button = context.browser.find_element(By.ID, "pto-link")
+    pto_button.click()
+
+
+@when("the user correctly fills out clock number")
+def step_impl(context):
+    """The test env correctly fills out the clock number"""
+    clock_input = context.browser.find_element(By.ID, "clock")
+    clock_input.send_keys("12345")
+
+
+@when("the user incorrectly fills out clock number")
+def step_impl(context):
+    """The test env incorrectly fills out the clock number"""
+    clock_input = context.browser.find_element(By.ID, "clock")
+    clock_input.send_keys("1")
+
+
+@when("the user correctly selects a shift")
+def step_impl(context):
+    """The user correctly selects a shift"""
+    shift_input = context.browser.find_element(By.ID, "shift")
+    shift_input.select_by_index(1)
+
+
+@when("the user correctly fills out their name")
+def step_impl(context):
+    """The user correctly fills out their name"""
+    name_input = context.browser.find_element(By.ID, "full-name")
+    name_input.send_keys("Testy McGee")
+
+
+@when("the user incorrectly fills out their name")
+def step_impl(context):
+    """The user incorrectly fills out their name.
+    In this case, it's an integer"""
+    name_input = context.browser.find_element(By.ID, "full-name")
+    name_input.send_keys(14)
+
+
+@when("the user correctly selects a start date")
+def step_impl(context):
+    """The user correctly selects a start date"""
+    date_input = context.browser.find_element(By.ID, "first-day-absent")
+    date_input.send_keys("12-20-2024")
+
+
+@when("the user incorrectly selects a start date")
+def step_impl(context):
+    """The user incorrectly selects a start date.
+    In this case, it's in the past."""
+    date_input = context.browser.find_element(By.ID, "first-day-absent")
+    date_input.send_keys("12-20-2001")
+
+
+@when("the user correctly selects a return date")
+def step_impl(context):
+    """The user correctly selects a return date."""
+    date_input = context.browser.find_element(By.ID, "last-day-absent")
+    date_input.send_keys("1-5-2025")
+
+
+@when("the user incorrectly selects a return date")
+def step_impl(context):
+    """The user incorrectly selects a return date.
+    In this case, it's in the past."""
+    date_input = context.browser.find_element(By.ID, "last-day-absent")
+    date_input.send_keys("1-5-2001")
+
+
+@when("the user correctly selects the amount of hours")
+def step_impl(context):
+    """The user correctly selects the amount of hours"""
+    hours_input = context.browser.find_element(By.ID, "hours")
+    hours_input.send_keys(136)
+
+
+@when("the user incorrectly selects the amount of hours")
+def step_impl(context):
+    """The user incorrectly selects the amount of hours,
+    in this case it's negative"""
+    hours_input = context.browser.find_element(By.ID, "hours")
+    hours_input.send_keys(-1)
+
+
+@when("the user correctly selects the reason for time off")
+def step_impl(context):
+    """The user correctly selects the reasoning for time off"""
+    reason_input = context.browser.find_element(By.ID, "absence-type")
+    reason_input.select_by_index(0)
+
+
+@when("the user correctly selects their approving supervisor")
+def step_impl(context):
+    """The user correctly selects the approving supervisor"""
+    super_input = context.browser.find_element(By.ID, "approving-supervisor")
+    super_input.send_keys("Supervisor McGee")
+
+
+@when("the user incorrectly selects their approving supervisor")
+def step_impl(context):
+    """The user incorrectly selects their approving supervisor"""
+    super_input = context.browser.find_element(By.ID, "approving-supervisor")
+    super_input.send_keys("Myself")
+
+
+@when("The user correctly enters their email")
+def step_impl(context):
+    """The user correctly enters their email"""
+    email_input = context.browser.find_element(By.ID, "email")
+    email_input.send_keys("testy_mcgee@test.com")
+
+
+@when("The user incorrectly enters their email")
+def step_impl(context):
+    """The user enters an incorrect email"""
+    email_input = context.browser.find_element(By.ID, "email")
+    email_input.send_keys("testy_mcgee@")
+
+
 @when("the user submits the login form with valid credentials")
 def step_impl(context):
-    """FILL IN"""
+    """Fills in the login form with valid credentials and submits it"""
     # context.browser.fill("username", "user")
     username_input = context.browser.find_element(By.ID, "username")
     username_input.send_keys("user")
@@ -33,6 +169,7 @@ def step_impl(context):
 
 @when("the user submits the login form with incorrect credentials")
 def step_impl(context):
+    """Fills in the login form with incorrect credentials and submits it"""
     username_input = context.browser.find_element(By.ID, "username")
     username_input.send_keys("user_wrong")
     password_input = context.browser.find_element(By.ID, "password")
@@ -43,12 +180,21 @@ def step_impl(context):
 
 @when("the user clicks on the PTO button")
 def step_impl(context):
+    """Clicks on the pto link"""
     pto_button = context.browser.find_element(By.ID, "pto-link")
     pto_button.click()
 
 
+@when("the user submits the PTO form")
+def step_impl(context):
+    """Submits the PTO form"""
+    pto_submit = context.browser.find_element(By.ID, "submit")
+    pto_submit.click()
+
+
 @then("the user should not be redirected to the home page")
 def step_impl(context):
+    """Checks if the test env is not on the home page"""
     assert context.browser.current_url != "http://localhost:8000/home", (
         f"Expected url to be on login page, "
         f"instead is on {context.browser.current_url}"
@@ -57,7 +203,7 @@ def step_impl(context):
 
 @then("the user should be redirected to the home page")
 def step_impl(context):
-    """FILL IN"""
+    """Checks if the test env is on the home page"""
     assert context.browser.current_url == "http://localhost:8000/home", (
         f"Expected url to be on home page, "
         f"instead is on {context.browser.current_url}"
@@ -66,6 +212,25 @@ def step_impl(context):
 
 @then("the user is redirected to the PTO form")
 def step_impl(context):
+    """Checks if the test env is on the PTO form"""
+    assert context.browser.current_url == "http://localhost:8000/pto", (
+        f"Expected url to be on pto form page, "
+        f"instead is on {context.browser.current_url}"
+    )
+
+
+@then("the form should be submitted")
+def step_impl(context):
+    """Checks if the test env is on the confirmation page"""
+    assert context.browser.current_url == "http://localhost:8000/confirm", (
+        f"Expected url to be on pto form page, "
+        f"instead is on {context.browser.current_url}"
+    )
+
+
+@then("the form should not be submitted")
+def step_impl(context):
+    """Checks if the test env is still on the PTO page"""
     assert context.browser.current_url == "http://localhost:8000/pto", (
         f"Expected url to be on pto form page, "
         f"instead is on {context.browser.current_url}"
