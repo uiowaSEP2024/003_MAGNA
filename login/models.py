5004f3c41649a8f4a672368c9871a8b6b122e340
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-02-13 22:07:45 -0600

    Django recommends using AbstractUser for the user model, so we're switching to that

--- login/models.py
+++ login/models.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Employee(models.Model):
+class Employee(AbstractUser):
     """Model for employees"""
 
     name = models.CharField(max_length=100)
     role = models.CharField(max_length=100)
     clock_number = models.CharField(max_length=10, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=15)
+    # email = models.EmailField(unique=True)
+    # password = models.CharField(max_length=15)
--- magna/settings.py
+++ magna/settings.py
@@ -107,6 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "login."
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
