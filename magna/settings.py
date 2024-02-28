2b341ce088dcdbd3b3cac615927d659a47229562
Charles Wilmot II <chazwilmot@gmail.com>
2024-02-28 14:05:47 -0600

    Revert "Fix DATABASES configuration in settings.py"
    This reverts commit b50df74d9a2848410ef7ab1ae3de2a34fa86e115.

--- magna/settings.py
+++ magna/settings.py
@@ -86,9 +86,8 @@ WSGI_APPLICATION = "magna.wsgi.application"
 #         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
+DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
