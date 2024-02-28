4d487a0ad644f5e7452cae2a98e75f0ad7bb7301
Chase Dittmer <chassedittmer@gmail.com>
2024-02-28 12:33:23 -0600

    added import statment that was not in my code

--- magna/urls.py
+++ magna/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 import forms.views
 import home.views
