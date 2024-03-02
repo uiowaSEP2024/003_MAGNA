599e085c23bbe7bca8b3f9c70cc16767c8e6b8ed
teckenrod <treyeck2@gmail.com>
2024-03-01 21:40:01 -0600

    Created file that can be run for setup up the environment and running of the server

---
+++ setup.py
@@ -0,0 +1,28 @@
+import subprocess
+
+
+def user_input():
+    """Prompt the user to flush and seed the database."""
+    while True:
+        response = input("Would you like to flush and seed the database? (y/n): ")
+        if response.lower() == "y":
+            return True
+        elif response.lower() == "n":
+            return False
+        else:
+            print("Invalid input. Please enter 'y' or 'n'")
+
+
+# Install poetry dependencies
+subprocess.run("poetry install", shell=True)
+
+# Run migrations
+subprocess.run("poetry run python manage.py migrate", shell=True)
+
+# Optionally flush and seed database
+if user_input():
+    subprocess.run("poetry run python manage.py flush", shell=True)
+    subprocess.run("poetry run python manage.py seed_employees", shell=True)
+
+# Run server
+subprocess.run("poetry run python manage.py runserver", shell=True)
