1207801b703281c6c4a546f6c630ae6b5601e352
Charles Wilmot II <chazwilmot@gmail.com>
2024-03-26 08:37:29 -0500

    WIP: Working on the models for workflows
    FormEmailSetting class

--- workflows/models.py
+++ workflows/models.py
@@ -15,3 +15,10 @@ class Workflow(models.Model):
     status = models.CharField(
         max_length=20, choices=STATUS_CHOICES, default="submitted"
     )
+
+class FormEmailSetting(models.Model):
+    form_type = models.CharField(max_length=100, unique=True)
+    email_recipients = models.TextField(help_text="Comma-separated list of email addresses")
+
+    def __str__(self):
+        return self.form_type
